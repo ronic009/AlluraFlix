bb41c1f1126518a1a7cbcc4a0515e090a032c5f1
ronic009 <77404768+ronic009@users.noreply.github.com>
2024-07-14 16:55:31 -0700

    Update videosData.js
    agregado id, image url correctamente

--- src/data/videosData.js
+++ src/data/videosData.js
@@ -1,52 +1,75 @@
 // src/data/videosData.js
 const videosData = {
-    "Front-End": [
-      { title: "Cuándo usar let var y const?", 
-        url: "https://youtu.be/PztCEdIJITY?si=YtfFzB3PEW_I9w4O",
-        image: "https://img.youtube.com/vi/PztCEdIJITY/maxresdefault.jpg", 
-        description: "Descripción del video" 
-      },
-      { title: "¿Qué es Javascript?", 
-        url: "https://youtu.be/GJfOSoaXk4s?si=ouMBfDIlgxN4yQn3",
-         image: "image-url.jpg", 
-         description: "Descripción del video" 
-      },
-      { title: "Equipo Front-End", 
-         url: "https://www.youtube.com/watch?v=rpvrLaBQwgg", 
-         image: "image-url.jpg",
-         description: "Descripción del video" 
-      },
-    ],
-    "Back-End": [
-      { title: "Spring Framework",
-        url: "https://www.youtube.com/watch?v=5XPojnx9bb8",
-        image: "image-url.jpg",
-        description: "Descripción del video"
-      },
-      { title: "¿Qué es SQL y NOSQL?",
-        url: "https://www.youtube.com/watch?v=aure5d3B88g",
-        image: "image-url.jpg", description: "Descripción del video" 
-      },
-      { title: "Simplificando tu código en JAVA . CONOCE los ENUM",
-        url: "https://www.youtube.com/watch?v=EoPvlE85XAQ",
-        image: "image-url.jpg", description: "Descripción del video" 
-      },
-    ],
-    "Innovación y Gestión": [
-      { title: "¿Qué son las Soft Skills?",
-        url: "https://www.youtube.com/watch?v=vhwspfvI52k",
-        image: "image-url.jpg", description: "Descripción del video" 
-      },
-      { title: "7 Soft Skills más deseadas por las empresas",
-        url: "https://www.youtube.com/watch?v=YhR7Zp8NUzE",
-        image: "image-url.jpg", description: "Descripción del video"
-      },
-      { title: "Metodologías Ágiles",
-        url: "https://www.youtube.com/watch?v=6N3OkLCfK-0", 
-        image: "image-url.jpg", description: "Descripción del video" 
-      },
-    ],
-  };
-  
-  export default videosData;
+  "Front-End": [
+    { 
+      id: 1,
+      title: "Cuándo usar let, var y const? #AluraMás", 
+      url: "https://youtu.be/PztCEdIJITY?si=YtfFzB3PEW_I9w4O",
+      image: "https://img.youtube.com/vi/PztCEdIJITY/default.jpg", 
+      description: "Descripción del video" 
+    },
+    { 
+      id: 2,
+      title: "¿Qué es Javascript?", 
+      url: "https://youtu.be/GJfOSoaXk4s?si=ouMBfDIlgxN4yQn3",
+      image: "https://img.youtube.com/vi/GJfOSoaXk4s/default.jpg", 
+      description: "Descripción del video" 
+    },
+    { 
+      id: 3,
+      title: "Equipo Front-End", 
+      url: "https://www.youtube.com/watch?v=rpvrLaBQwgg", 
+      image: "https://img.youtube.com/vi/rpvrLaBQwgg/default.jpg", // Corregido
+      description: "Descripción del video" 
+    },
+  ],
+  "Back-End": [
+    { 
+      id: 4,
+      title: "Spring Framework",
+      url: "https://www.youtube.com/watch?v=5XPojnx9bb8",
+      image: "https://img.youtube.com/vi/5XPojnx9bb8/default.jpg", // Corregido
+      description: "Descripción del video"
+    },
+    { 
+      id: 5,
+      title: "¿Qué es SQL y NOSQL?",
+      url: "https://www.youtube.com/watch?v=aure5d3B88g",
+      image: "https://img.youtube.com/vi/aure5d3B88g/default.jpg", // Corregido
+      description: "Descripción del video" 
+    },
+    { 
+      id: 6,
+      title: "Simplificando tu código en JAVA . CONOCE los ENUM",
+      url: "https://www.youtube.com/watch?v=EoPvlE85XAQ",
+      image: "https://img.youtube.com/vi/EoPvlE85XAQ/default.jpg", // Corregido
+      description: "Descripción del video" 
+    },
+  ],
+  "Innovación y Gestión": [
+    { 
+      id: 7,
+      title: "¿Qué son las Soft Skills?",
+      url: "https://www.youtube.com/watch?v=vhwspfvI52k",
+      image: "https://img.youtube.com/vi/vhwspfvI52k/default.jpg", // Corregido
+      description: "Descripción del video" 
+    },
+    { 
+      id: 8,
+      title: "7 Soft Skills más deseadas por las empresas",
+      url: "https://www.youtube.com/watch?v=YhR7Zp8NUzE",
+      image: "https://img.youtube.com/vi/YhR7Zp8NUzE/default.jpg", // Corregido
+      description: "Descripción del video"
+    },
+    { 
+      id: 9,
+      title: "Metodologías Ágiles",
+      url: "https://www.youtube.com/watch?v=6N3OkLCfK-0", 
+      image: "https://img.youtube.com/vi/6N3OkLCfK-0/default.jpg", // Corregido
+      description: "Descripción del video" 
+    },
+  ],
+};
+
+export default videosData;
   
